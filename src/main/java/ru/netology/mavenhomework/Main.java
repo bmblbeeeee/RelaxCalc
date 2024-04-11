bcbaf7d6d9c1bc8008a48fefe5a2dffab53c969e
Anastasia Valova <morugina.anast@mail.ru>
2024-04-11 21:22:46 +0300

    Add files

---
+++ src/main/java/ru/netology/mavenhomework/Main.java
@@ -0,0 +1,14 @@
+package ru.netology.mavenhomework;
+
+
+
+public class Main{
+    public static void main (String[] args){
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.mavenhomework.Calc;
+
+public class CalcTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void firstTest(int expected, int income, int expenses, int threshold){
+        Calc relax = new Calc();
+        int actual = relax.calculation(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+}
